5ba7b9a599d558cf7191546bb21a9d10f3e6c8d2
Yahir MT <yahirmt@hotmail.com>
2023-12-30 19:12:34 -0600

    Crear paginacion lista y eliminar post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        return view('posts.index', [
+            'posts' => Post::latest()->paginate()
+        ]);
+    }
+    
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return back();
     }
     //
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,26 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    Listado de aplicaciones
+                    <table class="mb-4">
+                        @foreach ($posts as $post)
+                            <tr class="border-b border-gray-200 text-sm">
+                                <td class="px-6 py-4">
+                                    {{$post->title}}
+                                </td>
+                                <td class="px-6 py-4">
+                                    <a href="http://" target="_blank" class="text-indigo-600">Editar</a>
+                                </td>
+                                <td class="px-6 py-4" id="">
+                                    <form action="{{route('posts.destroy', $post)}}" method="POST">
+                                        @csrf 
+                                        @method('DELETE')
+                                        <input type="submit" value="Eliminar" class="bg-gray-800 text-white rounded px-4 py-2" onclick="return confirm('Desea Eliminar?')">
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </table>
+                    {{$posts->links()}}
                 </div>
             </div>
         </div>
